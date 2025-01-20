e1d51ecbd0983d4567d59fbfdced565d32254542
TomH131 <twhill2010@hotmail.co.uk>
2025-01-20 09:15:51 +0000

    Added unique key value to plotly chart

--- src/machine_learning/predictive_analysis.py
+++ src/machine_learning/predictive_analysis.py
@@ -30,7 +30,9 @@ def plot_predictions_probabilities(pred_proba, pred_class):
         y=prob_per_class['Probability'],
         range_y=[0, 1],
         width=600, height=300, template='seaborn')
-    st.plotly_chart(fig)
+    
+    # Add a unique key to avoid Streamlit errors
+    st.plotly_chart(fig, key=f"plot_{pred_class}_{np.random.randint(1000)}")
 
 
 def resize_input_image(img, version):
